a157ffe41a59fd9decb388cdd8719d64568f8033
jmayta1984 <jmayta@pucp.edu.pe>
2025-09-09 13:18:29 -0500

    chore: initial project setup

---
+++ EasyShop/app/src/main/java/pe/edu/upc/easyshop/Main.kt
@@ -0,0 +1,74 @@
+package pe.edu.upc.easyshop
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import pe.edu.upc.easyshop.ui.theme.EasyShopTheme
+
+@Composable
+fun Main() {
+    val selectedIndex = remember {
+        mutableIntStateOf(0)
+    }
+
+    val navigationItems = listOf(
+        NavigationItem(icon = Icons.Default.Home, label = "Home"),
+        NavigationItem(icon = Icons.Default.Favorite, label = "Favorite"),
+        NavigationItem(icon = Icons.Default.ShoppingCart, label = "Cart"),
+        NavigationItem(icon = Icons.Default.Person, label = "Profile"),
+    )
+
+    Scaffold(
+        bottomBar = {
+            BottomAppBar {
+              navigationItems.forEachIndexed { index, item ->
+                  NavigationBarItem(
+                      selected = selectedIndex.intValue == index,
+                      icon = {
+                          Icon(item.icon, contentDescription = item.label)
+                      },
+
+                      label = {
+                          Text(text = item.label)
+                      },
+                      onClick = {
+                          selectedIndex.intValue = index
+                      }
+                  )
+              }
+            }
+        }
+
+    ) {
+        Column(modifier = Modifier.padding(it)) {
+
+        }
+
+    }
+}
+
+
+data class NavigationItem(val icon: ImageVector, val label: String)
+
+@Preview
+@Composable
+fun MainPreview() {
+    EasyShopTheme(dynamicColor = false) {
+        Main()
+    }
+}
