68f1bf7f14c3044546906906677192126ca26bc3
jmayta1984 <jmayta@pucp.edu.pe>
2025-09-30 13:32:49 -0500

    chore: update project

---
+++ EasyShop/app/src/main/java/pe/edu/upc/easyshop/MyApplication.kt
@@ -0,0 +1,15 @@
+package pe.edu.upc.easyshop
+
+import android.app.Application
+
+class MyApplication: Application() {
+
+    companion object {
+        lateinit var INSTANCE: MyApplication
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        INSTANCE = this
+    }
+}
