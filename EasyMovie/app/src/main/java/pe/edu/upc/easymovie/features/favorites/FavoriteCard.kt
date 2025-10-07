b7d5e71cea03eee3309ee9802d4b97c07f8fca6d
jmayta1984 <jmayta@pucp.edu.pe>
2025-10-07 15:32:24 -0500

    fix: organize imports

--- EasyMovie/app/src/main/java/pe/edu/upc/easymovie/features/favorites/FavoriteCard.kt
+++ EasyMovie/app/src/main/java/pe/edu/upc/easymovie/features/favorites/FavoriteCard.kt
@@ -9,8 +9,6 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
